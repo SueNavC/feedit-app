a2c8eab2987e99051a022279a739287e25daa52c
SueNavC <suenavarroc@gmail.com>
2024-10-26 17:25:14 +0200

    Trigger CI/CD workflow test

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -10,7 +10,7 @@ def analyze_sentiment_keywords(comment: str) -> str:
     """
     This function analyzes the sentiment of a comment based on the presence of certain keywords
     :param comment:
-    :return: "positive" if the comment contains "good" or "great", "negative" if it contains "bad" or "terrible", "neutral" otherwise
+    :return: "positive" if the comment contains "good" or "great", "negative" if it contains "bad" or "terrible".
     """
     if "good" in comment.lower() or "great" in comment.lower():
         return "positive"
@@ -23,7 +23,7 @@ def analyze_sentiment_textblob(comment: str) -> str:
     """
     This function analyzes the sentiment of a comment using TextBlob
     :param comment:
-    :return: "positive" if the polarity is greater than 0, "negative" if the polarity is less than 0, "neutral" otherwise
+    :return: "positive" if the polarity is greater than 0, "negative" if the polarity is less or equal than 0.
     """
     analysis = TextBlob(comment)
     polarity = analysis.sentiment.polarity
